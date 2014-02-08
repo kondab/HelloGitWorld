24f2af820529df268d5e6ea5671f38c07b85c6ae
KONDA BALAJI <balukonda@gmail.com>
2014-02-08 10:33:02 +0100

    Local to LOVE

--- Hello.java
+++
@@ -1,7 +0,0 @@
-public class Hello {
-	
-	public static void main (String[] args){
-		System.out.println("Hello Git HUB world1!");
-		System.out.println("Hello Git HUB world1!");
-	}
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-HelloGitWorld
-=============
-
-HelloGitWorld
